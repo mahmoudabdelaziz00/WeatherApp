828f2a508c127b6ad6ee83d62ef4f6255404d78a
DESKTOP <Mahmoud.m.abdelaziz00@gmail.com>
2025-03-04 14:09:03 +0200

    first commit

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:weather_app/model/weather_model.dart';
+
+class WeatherService {
+  final Dio dio;
+  final String baseUrl = 'http://api.weatherapi.com/v1';
+  final String apiKey = 'b616d44927a940ea9e363352252002';
+
+  WeatherService(this.dio);
+
+  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
+    Response response = await dio.get(
+        '$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=10');
+
+    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+
+    return weatherModel;
+  }
+}
