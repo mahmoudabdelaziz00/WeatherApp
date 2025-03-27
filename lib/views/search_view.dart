ab49be0aaceff51584bbe4837403b556aa615fa3
DESKTOP <Mahmoud.m.abdelaziz00@gmail.com>
2025-03-27 15:53:13 +0200

    first commit

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,11 +1,7 @@
-import 'dart:developer';
 
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
-import 'package:weather_app/model/weather_model.dart';
-import 'package:weather_app/services/weather_service.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
